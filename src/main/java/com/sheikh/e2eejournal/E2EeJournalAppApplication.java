b9fd6ecc74b79326d1eb53ba05aa42be2f662220
Sheikh Ahmed <sheikhishtiak@gmail.com>
2024-12-18 18:17:06 -0500

    Initial commit: Set up project structure

---
+++ src/main/java/com/sheikh/e2eejournal/E2EeJournalAppApplication.java
@@ -0,0 +1,13 @@
+package com.sheikh.e2eejournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2EeJournalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2EeJournalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sheikh/e2eejournal/E2EeJournalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheikh.e2eejournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2EeJournalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
